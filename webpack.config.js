cbd1e20944eddbb23264bff4a951c2d44bb04c0d
Sergey A Erkin <sergey.a.erkin@gmail.com>
2018-07-24 13:43:10 +0300

    #1 First setup new: webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  }
+};
